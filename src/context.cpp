7bd7f8a363c8a7c060b1fab2256c0228961e1e00
Dario Manesku <dariomanesku@gmail.com>
2015-04-10 20:21:26 +0200

    Changes for newset bgfx pull.

--- src/context.cpp
+++ src/context.cpp
@@ -1266,7 +1266,7 @@ namespace cs
                 mem = bgfx::makeRef(group.m_vertexData, group.m_vertexSize);
                 m_bufferHandles[ii].m_vbh = bgfx::createVertexBuffer(mem, m_decl);
 
-                const uint8_t flags = (group.m_32bitIndexBuffer ? BGFX_BUFFER_32BIT : 0);
+                const uint8_t flags = (group.m_32bitIndexBuffer ? BGFX_BUFFER_INDEX32 : 0);
                 mem = bgfx::makeRef(group.m_indexData, group.m_indexSize);
                 m_bufferHandles[ii].m_ibh = bgfx::createIndexBuffer(mem, flags);
             }
