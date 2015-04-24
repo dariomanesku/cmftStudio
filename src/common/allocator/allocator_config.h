6f8ee9ff857b7c54b25c0705aa384142d04a9884
Dario Manesku <dariomanesku@gmail.com>
2015-04-24 03:41:50 +0200

    Moved config implementation to allocator.cpp.

--- src/common/allocator/allocator_config.h
+++ src/common/allocator/allocator_config.h
@@ -11,8 +11,8 @@
 // Config.
 //-----
 
-#define CS_ALLOCATOR_IMPL_LIST  0 // Slower.
-#define CS_ALLOCATOR_IMPL_ARRAY 1 // Faster.
+#define CS_ALLOCATOR_IMPL_LIST  0 // Slower - left for testing purposes.
+#define CS_ALLOCATOR_IMPL_ARRAY 1 // Faster - recommended!
 
 #ifndef CS_ALLOCAOTR_IMPL
     #define CS_ALLOCAOTR_IMPL CS_ALLOCATOR_IMPL_ARRAY
@@ -56,59 +56,6 @@
 
 #ifndef CS_ALLOC_PRINT_BGFX
     #define CS_ALLOC_PRINT_BGFX 0
-#endif //CS_ALLOC_PRINT_BGFX
-
-// Implementation.
-//-----
-
-#if CS_ALLOC_PRINT_FILELINE
-    #define CS_ALLOC_FILE_LINE BX_FILE_LINE_LITERAL
-#else
-    #define CS_ALLOC_FILE_LINE
-#endif //CS_ALLOC_PRINT_FILELINE
-
-#define CS_ALLOC_PRINT(_format, ...) do { fprintf(stderr, "CS MEM " CS_ALLOC_FILE_LINE "" _format "\n", ##__VA_ARGS__); } while(0)
-
-#if CS_ALLOC_PRINT_STATS
-    #define CS_PRINT_MEM_STATS(_format, ...) CS_ALLOC_PRINT(_format, __VA_ARGS__)
-#else
-    #define CS_PRINT_MEM_STATS(...)
-#endif //CS_ALLOC_PRINT_STATS
-
-#if CS_ALLOC_PRINT_STATIC
-    #define CS_PRINT_STATIC(_format, ...) CS_ALLOC_PRINT(_format, __VA_ARGS__)
-#else
-    #define CS_PRINT_STATIC(...)
-#endif //CS_ALLOC_PRINT_STATIC
-
-#if CS_ALLOC_PRINT_SMALL
-    #define CS_PRINT_SMALL(_format, ...) CS_ALLOC_PRINT(_format, __VA_ARGS__)
-#else
-    #define CS_PRINT_SMALL(...)
-#endif //CS_ALLOC_PRINT_SMALL
-
-#if CS_ALLOC_PRINT_STACK
-    #define CS_PRINT_STACK(_format, ...) CS_ALLOC_PRINT(_format, __VA_ARGS__)
-#else
-    #define CS_PRINT_STACK(...)
-#endif //CS_ALLOC_PRINT_STACK
-
-#if CS_ALLOC_PRINT_HEAP
-    #define CS_PRINT_HEAP(_format, ...) CS_ALLOC_PRINT(_format, __VA_ARGS__)
-#else
-    #define CS_PRINT_HEAP(...)
-#endif //CS_ALLOC_PRINT_HEAP
-
-#if CS_ALLOC_PRINT_EXT
-    #define CS_PRINT_EXT(_format, ...) CS_ALLOC_PRINT(_format, __VA_ARGS__)
-#else
-    #define CS_PRINT_EXT(...)
-#endif //CS_ALLOC_PRINT_EXT
-
-#if CS_ALLOC_PRINT_BGFX
-    #define CS_PRINT_BGFX(_format, ...) CS_ALLOC_PRINT(_format, __VA_ARGS__)
-#else
-    #define CS_PRINT_BGFX(...)
 #endif //CS_ALLOC_PRINT_BGFX
 
 #endif // CMFTSTUDIO_ALLOCATOR_CONFIG_H_HEADER_GUARD
